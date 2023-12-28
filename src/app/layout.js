79e3023c9ea8f216055896ee1d3c9a7de3ec47d8
NPMA7 <pashamalik9371@gmail.com>
2023-12-28 11:48:37 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,11 +16,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
         <head>
-        <meta name="viewport" content="viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={inter.className}>
       <Head>
-        <meta name="viewport" content="viewport-fit=cover" />
+       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
         <div >
           <Header/>
