b759ac72b1ff59d57b9194a8918ec55d3625db50
NPMA7 <pashamalik9371@gmail.com>
2023-12-28 15:05:32 +0800

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,14 +10,14 @@ const Footer = () => {
                 className="h-8 me-3"
                 alt="FlowBite Logo"
               />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-blue-600 tracking-widest">
+              <span className="self-center text-2xl font-semibold whitespace-nowrap text-blue-600 tracking-widest">
                 Arnisekai
               </span>
             </a>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase ">
                 Resources
               </h2>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
