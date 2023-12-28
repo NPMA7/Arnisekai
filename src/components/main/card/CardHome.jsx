45db39b83a89daf198ec228197c6cfaabdc6b628
NPMA7 <pashamalik9371@gmail.com>
2023-12-28 16:54:36 +0800

    text

--- src/components/main/card/CardHome.jsx
+++ src/components/main/card/CardHome.jsx
@@ -69,13 +69,13 @@ export const CardAnimeOngoing = ({
             />
           </div>
           <div
-            className="pr-6 pt-52 text-gray-100 max-[768px]:pt-40
+            className="pr-2 pt-52 text-gray-100 max-[768px]:pt-52
             absolute "
           >
             🌟{rating}
           </div>
           <div
-            className="mt-1 w-28 pl-3.5 pt-60 text-gray-100 max-[768px]:pt-44
+            className="mt-1 w-28 pl-3.5 pt-60 text-gray-100 max-[768px]:pt-60
           absolute "
           >
             {episode_complete} Episode
