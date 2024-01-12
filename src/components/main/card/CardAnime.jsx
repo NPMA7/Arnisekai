6dda4ffa2202207718ae27ed17a949d67715b566
NPMA7 <pashamalik9371@gmail.com>
2024-01-12 22:37:04 +0800

    test3

--- src/components/main/card/CardAnime.jsx
+++ src/components/main/card/CardAnime.jsx
@@ -3,40 +3,32 @@ export const CardAnimeOngoing = ({
   poster_anime,
   episode_baru,
   episode_rilis,
-
 }) => {
   return (
     <div className="">
-      <div className="hover:text-gray-30 flex justify-center cursor-pointer">
+      <div className="ml-4">
         <div
-          className="absolute pt-2 h-14 w-32 hover:h-36 text-center overflow-hidden"
-        >
-          {title_anime}
-        </div>
-       
-        <div className="border-4 text-center rounded-md border-blue-400 ">
-          <img
-            id="image-card-anime"
-            src={poster_anime}
-          />
-           <div
-          className=" mt-52 text-gray-100  
+          className=" mt-40 text-gray-100  
         absolute max-xs:text-sm"
         >
           {episode_rilis}
         </div>
         <div
-          className=" mt-56 text-gray-100
+          className=" mt-44 text-gray-100
         absolute max-xs:text-sm"
         >
-          {episode_baru} 
+          {episode_baru}
         </div>
+      </div>
+      <div className="hover:text-gray-30 flex justify-center cursor-pointer">
+        <div className="absolute pt-2 h-14 w-32 hover:h-36 text-center overflow-hidden">
+          {title_anime}
         </div>
-       
+        <div className="border-4 text-center rounded-md border-blue-400 ">
+          <img id="image-card-anime" src={poster_anime} />
+        </div>
       </div>
-      <div>
-        
-      </div>
+      
     </div>
   );
 };
@@ -49,38 +41,30 @@ export const CardAnimeComplete = ({
 }) => {
   return (
     <div className="">
-      <div className="hover:text-gray-30 flex justify-center cursor-pointer">
-        <div
-          className="absolute pt-2 h-14 w-32 hover:h-36 text-center overflow-hidden"
-        >
-          {title_anime}
-        </div>
-     
-        <div
-          className=" mt-56 text-gray-100
-        absolute max-xs:text-sm"
-        >
-          {episode_complete} Episode
-        </div>
-        <div className="border-4 rounded-md border-blue-400 ">
-          <img
-            id="image-card-anime"
-            src={poster_anime}
-          />
-             <div
-          className=" mt-52 text-gray-100 
+      <div className="pl-4">
+      <div
+          className=" mt-40 text-gray-100 
         absolute max-xs:text-sm"
         >
           ⭐{rating}
         </div>
+        <div
+          className=" mt-44 text-gray-100
+        absolute max-xs:text-sm"
+        >
+          {episode_complete} Episode
+        </div>
+      </div>
+      <div className="hover:text-gray-30 flex justify-center cursor-pointer">
+        <div className="absolute pt-2 h-14 w-32 hover:h-36 text-center overflow-hidden">
+          {title_anime}
         </div>
         
+        <div className="border-4 rounded-md border-blue-400 ">
+          <img id="image-card-anime" src={poster_anime} />
+        </div>
       </div>
-      <div>
-        
-      </div>
+      <div></div>
     </div>
   );
 };
-
-
